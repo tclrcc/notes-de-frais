5663e77017308a1ea0b3a9bf48b6952a13766cea
Tony Coloricchio <tonycoloricchio01@gmail.com>
2026-09-26 18:00:02 +0200

    fix(test): empêche l'IT de rejoindre les consumer groups applicatifs

--- src/test/java/fr/coloricchio/notesdefrais/NotesDeFraisApplicationIT.java
+++ src/test/java/fr/coloricchio/notesdefrais/NotesDeFraisApplicationIT.java
@@ -3,7 +3,7 @@ package fr.coloricchio.notesdefrais;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.kafka.listener.auto-startup=false")
 class NotesDeFraisApplicationIT {
 
     @Test
